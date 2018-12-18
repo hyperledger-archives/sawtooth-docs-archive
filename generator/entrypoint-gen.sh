f135702c32351b70560ff3150193240f9fd577f1
Sutrannu <bbetts@bitwise.io>
2018-12-18 10:46:53 -0600

    Improve nightly branch iteration and add branches
    Signed-off-by: Sutrannu <bbetts@bitwise.io>

--- generator/entrypoint-gen.sh
+++ generator/entrypoint-gen.sh
@@ -49,30 +49,36 @@ info "Extracting docs"
 mkdir -p ./archive/docs
 ls ./archive/core/*.gz | xargs -I{} tar --skip-old-files -xzf {} -C ./archive/docs/
 
+# Artifacts are retrieved from build.sawtooth.me for each repo/branch
+# Jenkins creates these with each change to the branch
+# The buildlist is in the format, repo:branch
+# NOTE: update /source/docs/versions.json and /source/docs/docs.rst
 info "Retrieving nightlies"
-for repo in core seth raft sabre supply-chain; do
-    for branch in master; do
-        remote_epoch=$(nightly_epoch "$repo" "$branch")
-        archive_epoch=$(stat -c '%Y' \
-            ./archive/nightly/${repo}/${branch}/html.zip 2>/dev/null \
-            || echo 0 | tr -cd '0-9')
-        # If the file has not been updated, to not download.
-        if [ $remote_epoch -gt $archive_epoch ]; then
-            info "  fetching ${repo}/${branch}"
-            url="$(jenkins_build_url "$repo" "$branch")artifact/docs/build/html/*zip*/html.zip"
-            mkdir -p ./archive/nightly/${repo}/${branch}
-            cd ./archive/nightly/${repo}/${branch}
-            wget -qN --no-check-certificate "${url}"
-            cd $wd
-            unzip -qn ./archive/nightly/${repo}/${branch}/html.zip \
-                -d ./archive/nightly/${repo}/
-            mkdir -p ./archive/docs/${repo}/nightly/${branch}/
-            cp -rd ./archive/nightly/${repo}/html/* \
-                ./archive/docs/${repo}/nightly/${branch}/
-        else
-            info "  ${repo}/${branch} up do date"
-        fi
-    done
+buildlist="core:master core:1-1 seth:master raft:master sabre:master \
+    supply-chain:master pbft:master"
+for build in $buildlist; do
+    repo=$(echo $build | cut -d: -f1)
+    branch=$(echo $build | cut -d: -f2)
+    remote_epoch=$(nightly_epoch "$repo" "$branch")
+    archive_epoch=$(stat -c '%Y' \
+        ./archive/nightly/${repo}/${branch}/html.zip 2>/dev/null \
+        || echo 0 | tr -cd '0-9')
+    # If the file has not been updated, to not download.
+    if [ $remote_epoch -gt $archive_epoch ]; then
+        info "  fetching ${repo}/${branch}"
+        url="$(jenkins_build_url "$repo" "$branch")artifact/docs/build/html/*zip*/html.zip"
+        mkdir -p ./archive/nightly/${repo}/${branch}
+        cd ./archive/nightly/${repo}/${branch}
+        wget -qN --no-check-certificate "${url}"
+        cd $wd
+        unzip -qn ./archive/nightly/${repo}/${branch}/html.zip \
+            -d ./archive/nightly/${repo}/
+        mkdir -p ./archive/docs/${repo}/nightly/${branch}/
+        cp -rd ./archive/nightly/${repo}/html/* \
+            ./archive/docs/${repo}/nightly/${branch}/
+    else
+        info "  ${repo}/${branch} up do date"
+    fi
 done
 
 info "Setting symlinks"
--- generator/source/docs/docs.rst
+++ generator/source/docs/docs.rst
@@ -25,7 +25,7 @@ Sawtooth Core
 -------------
 
 -  `Latest Release <core/releases/latest/>`__
--  `Release 1.1 (Bumper) <core/releases/1.1.2/>`__
+-  `Release 1.1.2 <core/releases/1.1.2/>`__
 -  `Release 1.0.5 <core/releases/1.0.5/>`__
 -  `Nightly master <core/nightly/master/>`__
 -  `Nightly 1-1 <core/nightly/1-1/>`__
@@ -33,13 +33,11 @@ Sawtooth Core
 Sawtooth Sabre
 --------------
 
--  `Latest Release <sabre/releases/latest/>`__
 -  `Nightly master <sabre/nightly/master/>`__
 
 Sawtooth Seth
 -------------
 
--  `Latest Release <seth/releases/latest/>`__
 -  `Nightly master <seth/nightly/master/>`__
 
 Sawtooth Supply Chain
