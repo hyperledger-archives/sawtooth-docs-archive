ee92365aa1041ba1212052cf5270d54a592473ba
Ryan Beck-Buysse <rbuysse@bitwise.io>
2019-04-03 15:43:54 -0500

    Add JS and Rust sdk docs to the website
    Docs will be published to the website but not linked yet
    
    Signed-off-by: Ryan Beck-Buysse <rbuysse@bitwise.io>

--- generator/entrypoint-gen.sh
+++ generator/entrypoint-gen.sh
@@ -55,7 +55,8 @@ ls ./archive/core/*.gz | xargs -I{} tar --skip-old-files -xzf {} -C ./archive/do
 # NOTE: update /source/docs/versions.json and /source/docs/docs.rst
 info "Retrieving nightlies"
 buildlist="core:master core:1-1 seth:master raft:master sabre:master \
-    supply-chain:master pbft:master sdk-python:master"
+    supply-chain:master pbft:master sdk-javascript:master \
+    sdk-python:master sdk-rust:master"
 for build in $buildlist; do
     repo=$(echo $build | cut -d: -f1)
     branch=$(echo $build | cut -d: -f2)
